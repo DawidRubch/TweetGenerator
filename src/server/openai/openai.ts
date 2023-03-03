e74323a6ce6bc23557db533e60a6fc1898ef99a1
DawidRubch <dawid.niegrebecki@gmail.com>
2023-03-03 21:36:28 +0100

    Migrated from davincii model to gpt-3.5-turbo

--- src/server/openai/openai.ts
+++ src/server/openai/openai.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { env } from "../../env/server.mjs";
 
-type OpenAIResponse = {
+type DavinciModelResponse = {
   data: {
     choices: {
       text: string;
@@ -9,27 +9,59 @@ type OpenAIResponse = {
   };
 };
 
+type ChatGPTModelResponse = {
+  data: {
+    choices: {
+      index: 0;
+      message: {
+        role: string;
+        content: string;
+      };
+    }[];
+  };
+};
+
 export const openai = axios.create({
-  baseURL: "https://api.openai.com/v1/completions",
+  baseURL: "https://api.openai.com/v1",
   headers: { Authorization: `Bearer ${env.OPEN_AI_SECRET}` },
 });
 
+const getTweetIdeasUsingDavinciModel = async (input: string) => {
+  const response: DavinciModelResponse = await openai.post("/completions", {
+    model: "text-davinci-003",
+    prompt: `Generate 5 tweets about: ${input}. Use a number list to separete them.`,
+    max_tokens: 200,
+    temperature: 1,
+  });
+
+  const tweetIdeas = response.data.choices[0]?.text;
+
+  return mapTweetIdeas(tweetIdeas ?? "");
+};
+
+const getTweetIdeasUsingChatGPTModel = async (input: string) => {
+  const response: ChatGPTModelResponse = await openai.post(
+    "/chat/completions",
+    {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Generate 5 tweets about: ${input}. Use a number list to separete them.`,
+        },
+      ],
+    }
+  );
+
+  const tweetIdeas = response.data.choices[0]?.message.content;
+
+  return mapTweetIdeas(tweetIdeas ?? "");
+};
+
 export const generateTweetIdeas = async (input: string) => {
-  try {
-    const response: OpenAIResponse = await openai.post("", {
-      model: "text-davinci-003",
-      prompt: `Generate 5 tweets about: ${input}. Use a number list to separete them.`,
-      max_tokens: 200,
-      temperature: 1,
-    });
+  const tweetIdeas = await getTweetIdeasUsingChatGPTModel(input);
 
-    const tweetIdeas = response.data.choices[0]?.text;
-
-    return mapTweetIdeas(tweetIdeas ?? "");
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
+  return tweetIdeas;
 };
 
 export const mapTweetIdeas = (tweetIdeas: string) => {
